f00741c493085c27dfe528caa88ee21b71b59d02
Jeayeong_Hong <hong612644@gmail.com>
2026-02-04 17:21:17 +0900

    반응형 ui 및 ui 최종 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -118,7 +118,22 @@ export default function Home() {
             onClick={handleAddItem}
             disabled={isSubmitting || !isAddButtonActive}
           >
-            <img src="/icon/add_btn_plus.svg" alt="" />
+            {/* +icon색상 변경 */}
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                d="M12 5v14M5 12h14"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+            </svg>
+
             {!isMobile && "추가하기"}
           </ImageButton>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useMediaQuery } from "react-responsive";
 
 // 상단 로고 헤더 컴포넌트
@@ -9,13 +10,15 @@ export default function Header() {
 
     return (
         <header className="header">
-            <Image
-                src={isMobile ? "/Img/top_cha.svg" : "/Img/top_cha.svg"}
-                alt="do it ;"
-                width={isMobile ? 60 : 120}
-                height={isMobile ? 45 : 90}
-                style={{ width: 'auto', height: 'auto' }}
-            />
+            <Link href="/" style={{ cursor: 'pointer' }}>
+                <Image
+                    src={isMobile ? "/Img/top_cha.svg" : "/Img/top_cha.svg"}
+                    alt="do it ;"
+                    width={isMobile ? 60 : 120}
+                    height={isMobile ? 45 : 90}
+                    style={{ width: 'auto', height: 'auto' }}
+                />
+            </Link>
         </header>
     );
 }
